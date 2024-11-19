20db31b4961f3853abe0eaff3af8de9476222583
Jan Kaiser <jan.kaiser.email@googlemail.com>
2024-11-19 17:46:30 +0100

    Refactor

--- paper2movie.py
+++ paper2movie.py
@@ -1,4 +1,6 @@
 import math
+import os
+import subprocess
 from pathlib import Path
 
 import git
@@ -20,7 +22,6 @@ filename = "main"
 
 # Path to output folder
 output_path = Path.home() / "Desktop" / "papermovie_output" / "output"
-output_path.mkdir(parents=True, exist_ok=True)
 
 # Video size (by default Full HD)
 total_width = 3840
@@ -31,136 +32,151 @@ total_height = 2160
 # It will give a better results, since there is much more to look at. ;-)
 fps = 2
 
-############################
-### RETRIEVING PDF FILES ###
-############################
 
-repo = git.Repo(input_path)
+def generate_pdfs(repo: git.Repo) -> None:
+    """
+    Generate PDFs by looping through all past commits on the brange and rendering the
+    LaTeX project.
+    """
+    output_path.mkdir(parents=True, exist_ok=True)
+    last_commit = list(repo.iter_commits(branch))[-1]
 
-num_commits = len(list(repo.iter_commits(branch)))
-last_commit = list(repo.iter_commits(branch))[-1]
-ic(last_commit)
+    for i, commit in tqdm(
+        list(enumerate(repo.iter_commits(branch)))[:3], desc="Generating PDFs ..."
+    ):
+        # Check out old commit
+        repo.git.checkout(commit)
 
-max_page_num = 0
+        # Compile tex with references
+        repo.git.execute(
+            ["pdflatex", "-synctex=1", "-interaction=nonstopmode", f"{filename}.tex"]
+        )
+        repo.git.execute(["bibtex", f"{filename}.aux"])
+        repo.git.execute(
+            ["pdflatex", "-synctex=1", "-interaction=nonstopmode", f"{filename}.tex"]
+        )
+        repo.git.execute(["bibtex", f"{filename}.aux"])
+        repo.git.execute(
+            ["pdflatex", "-synctex=1", "-interaction=nonstopmode", f"{filename}.tex"]
+        )
 
-print("Start retrieving PDFs")
-ic(list(enumerate(repo.iter_commits(branch)))[:3])
-for i, commit in tqdm(list(enumerate(repo.iter_commits(branch)))[:3]):
-    print(f"Retrieving PDF revision {i+1}/{num_commits} ...")
+        # Move generated pdf to output folder
+        (input_path / f"{filename}.pdf").rename(output_path / f"{i+1:03d}.pdf")
 
-    # Check out old commits
-    ic(commit)
-    repo.git.checkout(commit)
+    # Revert back to last commit
+    repo.git.checkout(last_commit)
 
-    # Compile tex with references
-    repo.git.execute(
-        ["pdflatex", "-synctex=1", "-interaction=nonstopmode", f"{filename}.tex"]
-    )
-    repo.git.execute(["bibtex", f"{filename}.aux"])
-    repo.git.execute(
-        ["pdflatex", "-synctex=1", "-interaction=nonstopmode", f"{filename}.tex"]
-    )
-    repo.git.execute(["bibtex", f"{filename}.aux"])
-    repo.git.execute(
-        ["pdflatex", "-synctex=1", "-interaction=nonstopmode", f"{filename}.tex"]
-    )
 
-    # Find maximum number of pages
-    pages = repo.git.execute(["pdfinfo", f"{filename}.pdf"]).split("\n")
-    pages = [int(page.split()[1]) for page in pages if page.startswith("Pages")][0]
-    if pages > max_page_num:
-        max_page_num = pages
+def find_maximum_number_of_pages(output_path):
+    """Find maximum number of pages in the generated PDFs."""
+    print("Finding maximum number of pages ...", end=" ")
+    max_page_num = 0
+    for pdf in output_path.glob("*.pdf"):
+        pages = subprocess.run(
+            ["pdfinfo", pdf], stdout=subprocess.PIPE, text=True
+        ).stdout.split("\n")
+        pages = [int(page.split()[1]) for page in pages if page.startswith("Pages")][0]
+        if pages > max_page_num:
+            max_page_num = pages
 
-    # Move pdf to output folder
-    format_filename = f"{i+1:03d}"
-    repo.git.execute(["cp", f"{filename}.pdf", f"{output_path / format_filename}.pdf"])
+    print(f"{max_page_num}")
 
-# Revert back to last commit
-repo.git.checkout(last_commit)
+    return max_page_num
 
-############################
-###    CREATING IMAGES   ###
-############################
 
-# The width/height ratio of A4 paper
-ratio_a4 = 0.7070
+def compute_tile_sizes(total_width, total_height, max_page_num):
+    """Compute the title size and numbers of tiles to fit into the image."""
+    print("Computing tile sizes ...")
 
-# The height of an individual tile
-tile_height = (total_width * total_height / (ratio_a4 * max_page_num)) ** 0.5
-tile_width = tile_height * ratio_a4
-ic(tile_height, tile_width)
+    # The width/height ratio of A4 paper
+    ratio_a4 = 0.7070
 
-# Calculate grid
-num_tiles_height = total_height / tile_height
-num_tiles_width = total_width / tile_width
-ic(num_tiles_height, num_tiles_width)
+    # The width and height of an individual tile
+    tile_height = (total_width * total_height / (ratio_a4 * max_page_num)) ** 0.5
+    tile_width = tile_height * ratio_a4
 
-# Ceil tiles to integers
-num_tiles_height = math.ceil(num_tiles_height)
-num_tiles_width = math.ceil(num_tiles_width)
-ic(num_tiles_height, num_tiles_width)
+    # Calculate grid
+    grid_height = math.ceil(total_height / tile_height)
+    grid_width = math.ceil(total_width / tile_width)
 
-# Report measurements
-print("Movie measurements:")
-print(f"Number of horizontal tiles: {num_tiles_width}")
-print(f"Number of vertical tiles: {num_tiles_height}")
+    # Have ceiled the number of tiles, they exceed the total_width and total_height.
+    # So, we also need to recalculate the tile_height and tile_width. This step will
+    # (slightly) change the A4 ratio, but it beats half pages in the video.
+    tile_height = total_height / grid_height
+    tile_width = total_width / grid_width
 
-# Have ceiled the number of tiles, they exceed the total_width and total_height.
-# So, we also need to recalculate the tile_height and tile_width. This step will
-# (slightly) change the A4 ratio, but it beats half pages in the video.
-tile_height = total_height / num_tiles_height
-tile_width = total_width / num_tiles_width
-ic(tile_height, tile_width)
+    print(f"    ... Grid size: {grid_width} x {grid_height}")
+    print(f"    ... Tile size: {tile_width} x {tile_height}")
 
-# Generate images
-print("Start generating images ...")
-for i in tqdm(range(3)):  # num_commits)):
-    format_filename = f"{i+1:03d}"
-    repo.git.execute(
+    return grid_width, grid_height, tile_width, tile_height
+
+
+def generate_images(
+    output_path, num_tiles_width, num_tiles_height, tile_width, tile_height
+):
+    """Generate images from PDFs."""
+    for i in tqdm(range(3), desc="Generating images ..."):
+        # num_commits)):
+        filename = f"{i+1:03d}"
+        subprocess.run(
+            [
+                "montage",
+                f"{output_path / filename}.pdf",
+                "-tile",
+                f"{num_tiles_width}x{num_tiles_height}",
+                "-background",
+                "white",
+                "-geometry",
+                f"{tile_width}x{tile_height}",
+                "-alpha",
+                "remove",
+                "-colorspace",
+                "sRGB",
+                f"{output_path / filename}.png",
+            ]
+        )
+
+
+def render_movie():
+    """Render movie from images."""
+    print("Rendering movie ...")
+    subprocess.run(
         [
-            "montage",
-            f"{output_path / format_filename}.pdf",
-            "-tile",
-            f"{num_tiles_width}x{num_tiles_height}",
-            "-background",
-            "white",
-            "-geometry",
-            f"{tile_width}x{tile_height}",
-            "-alpha",
-            "remove",
-            "-colorspace",
-            "sRGB",
-            f"{output_path / format_filename}.png",
+            "ffmpeg",
+            "-r",
+            f"{fps}",
+            "-i",
+            f"{output_path / '%03d.png'}",
+            "-pix_fmt",
+            "yuv420p",
+            "-b",
+            "8000k",
+            "-vcodec",
+            "libx264",
+            f"{output_path / filename}.mp4",
         ]
     )
-    print(f"Image #{format_filename} generated!")
 
-############################
-###     RENDER MOVIE     ###
-############################
 
-print("Start rendering movie ...")
-repo.git.execute(
-    [
-        "ffmpeg",
-        "-r",
-        f"{fps}",
-        "-i",
-        f"{output_path / '%03d.png'}",
-        "-pix_fmt",
-        "yuv420p",
-        "-b",
-        "8000k",
-        "-vcodec",
-        "libx264",
-        f"{output_path / filename}.mp4",
-    ]
-)
+def main():
+    repo = git.Repo(input_path)
 
-############################
-###     CLEANUP STUFF    ###
-############################
+    generate_pdfs(repo)
+    max_page_num = find_maximum_number_of_pages(output_path)
+    grid_width, grid_height, tile_width, tile_height = compute_tile_sizes(
+        total_width, total_height, max_page_num
+    )
+    generate_images(output_path, grid_width, grid_height, tile_width, tile_height)
+    render_movie()
 
-print("Movie rendered, cleaning up ... or not")
+    ############################
+    ###     CLEANUP STUFF    ###
+    ############################
 
-print(f"Movie available at {output_path / filename}.mp4")
+    print("Movie rendered, cleaning up ... or not")
+
+    print(f"Movie available at {output_path / filename}.mp4")
+
+
+if __name__ == "__main__":
+    main()
